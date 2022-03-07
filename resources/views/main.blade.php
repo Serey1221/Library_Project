92ce0c5b8e51b5ee2a50e6a762a47860cb9ab60a
serey1221 <sereysrout93@gmail.com>
2022-03-07 20:32:26 +0700

    update CRUD

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,7 +17,7 @@
     <link href="{{ URL::asset('vendor/fontawesome-free/css/all.min.css'); }}" rel="stylesheet" type="text/css">
 </head>
 <body id="page-top">
-<div id="wrapper">
+    <div id="wrapper">
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     
             <!-- Sidebar - Brand -->
@@ -329,106 +329,11 @@
 
                     </ul>
                 </nav>
-                <div class="container">
-                    <form action="/authors" method="POST" class="row justify-content-center">
-                        <div class="col-lg-6">
-                            @csrf
-                            <div class="m-2">
-                                <div class="form-group ">
-                                    <label  for="name">Name</label>
-                                    <input class="form-control @error('name') is-invalid @enderror" type="text" 
-                                    placeholder="Enter name" 
-                                        name="name" id="name" value="{{ old('name') }}" />
-                                        @error('name')
-                                        <span class="text-danger">{{$message}}</span>
-                                        @enderror 
-                                        
-                                </div>
-                            </div>
-                            <div class="m-2">
-                                <div class="form-group">
-                                    <label  for="gender">Gender</label>
-                                    <input class="form-control" type="text" placeholder="Enter Gender" 
-                                        name="gender" id="gender" value="{{ old('gender') }}" />
-                                        @error('gender')
-                                        <span class="text-danger">{{$message}}</span>
-                                        @enderror
-                                </div>
-                            </div>
-                            <div class="m-2">
-                                <div class="form-group">
-                                    <label  for="dob">Date of Birth</label>
-                                    <input class="form-control" type="dob" placeholder="Enter dob" 
-                                        name="dob" id="dob" value="{{ old('dob') }}" />
-                                        @error('dob')
-                                        <span class="text-danger">{{$message}}</span>
-                                        @enderror
-                                </div>
-                            </div>
-                            <div class="m-2">
-                                <div class="form-group">
-                                    <label  for="pob">POB</label>
-                                    <input class="form-control" type="text" placeholder="Enter pob " 
-                                        name="pob" id="pob" value="{{ old('pob') }}" />
-                                        @error('pob')
-                                        <span class="text-danger">{{$message}}</span>
-                                        @enderror
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="m-2">
-                                <div class="form-group">
-                                    <label  for="address">Address</label>
-                                    <input class="form-control" type="text" placeholder="Enter address" 
-                                        name="address" id="address" value="{{ old('address') }}" />
-                                        @error('address')
-                                        <span class="text-danger">{{$message}}</span>
-                                        @enderror
-                                </div>
-                            </div>
-                            <div class="m-2">
-                                <div class="form-group">
-                                    <label  for="phone">Phone</label>
-                                    <input class="form-control" type="text" placeholder="Enter phone" 
-                                        name="phone" id="phone" value="{{ old('phone') }}" ></input>
-                                        @error('phone')
-                                        <span class="text-danger">{{$message}}</span>
-                                        @enderror
-                                </div>
-                            </div>
-                            <div class="m-2">
-                                <div class="form-group">
-                                    <label  for="email">Email</label>
-                                    <input class="form-control" type="text" placeholder="Enter email" 
-                                        name="email" id="email" value="{{ old('email') }}" />
-                                        @error('email')
-                                        <span class="text-danger">{{$message}}</span>
-                                        @enderror
-                                </div>
-                            </div>
-                            <div class="m-2">
-                                <div class="form-group">
-                                    <label  for="photo">Photo</label>
-                                    <input class="form-control" type="text" placeholder="Enter photo" 
-                                        name="photo" id="photo" value="{{ old('photo') }}" />
-                                        @error('photo')
-                                        <span class="text-danger">{{$message}}</span>
-                                        @enderror
-                                </div>
-                            </div>
-                                <div class="m-2">
-                                    <input class="btn btn-success" type="submit" value="Create">
-                                    <a class="btn btn-info" href="/authors">Back</a>
-                                </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                
         </div>
     </div>
 </body>
-<script src="{{ URL::asset('vendor/jquery/jquery.min.js'); }}"></script>
+    <script src="{{ URL::asset('vendor/jquery/jquery.min.js'); }}"></script>
     <script src="{{ URL::asset('vendor/bootstrap/js/bootstrap.bundle.min.js'); }}"></script>
     <script src="{{ URL::asset('vendor/jquery-easing/jquery.easing.min.js'); }}"></script>
     <script src="{{ URL::asset('js/sb-admin-2.min.js'); }}"></script>
@@ -436,3 +341,5 @@
     <script src="{{ URL::asset('js/demo/chart-area-demo.js'); }}"></script>
     <script src="{{ URL::asset('js/demo/chart-pie-demo.js'); }}"></script>
 </html>
+
+
